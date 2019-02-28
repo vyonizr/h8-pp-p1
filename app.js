40796f99cf2a5db3178fb5c425a465e107c0b1cc
hermsz <email@hermi.ltd>
2019-02-28 15:56:16 +0700

    Updated Login and Register

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const routes = require("./routes")
 app.set("view engine", "ejs")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('css'))
 
 const indexRouter = require('./routes/index')
+const loginRouter = require('./routes/loginRouter')
+const signUpRouter = require('./routes/registerRouter')
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
+app.use('/login', loginRouter)
+app.use('/register', signUpRouter)
+
 
 app.listen(3000)
--- models/consumer.js
+++ models/consumer.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Consumer = sequelize.define('Consumer', {
     username: DataTypes.STRING,
-    first_name: DataTypes.STRING,
+    full_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     password: DataTypes.STRING,
     email: DataTypes.STRING,
