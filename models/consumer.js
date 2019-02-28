27a623234f0346cbc270fddbb20022f8023e43b9
hermsz <email@hermi.ltd>
2019-02-28 16:01:29 +0700

    Accept conflict

--- models/consumer.js
+++ models/consumer.js
@@ -1,15 +1,49 @@
 'use strict';
+const bcrypt = require("bcryptjs")
+const fixNameCase = require("../helpers/fixNameCase")
+
 module.exports = (sequelize, DataTypes) => {
   const Consumer = sequelize.define('Consumer', {
-    username: DataTypes.STRING,
-    full_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        unique: true,
+        isOneWord(value) {
+          var regexp = /^\S*$/
+          if(!regexp.test(value)) {
+            throw new Error("Must not contain any spaces")
+          }
+        }
+      }
+    },
+    full_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+        unique: true
+      }
+    },
     gender: DataTypes.ENUM("male", "female")
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate(consumer, options) {
+        consumer.full_name = fixNameCase(full_name)
+        consumer.gender = consumer.gender.toLowerCase()
+      }
+    }
+  });
   Consumer.associate = function(models) {
     // associations can be defined here
+    Consumer.hasMany(models.Booking)
   };
   return Consumer;
 };
